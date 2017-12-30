ceb7ddcc143b52e770fbbfda9925ca25586cad99
mahesh.bavanam@gmail.com <mahesh.bavanam@gmail.com>
2017-12-30 15:05:19 -0700

    Db url change

--- ds/connect.js
+++ ds/connect.js
@@ -1,2 +1,3 @@
 var mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost:27017/UJJWALA_HOME_DB");
+//mongoose.connect("mongodb://localhost:27017/UJJWALA_HOME_DB");
+mongoose.connect("mongodb://bmreddy573:bmreddy573@ds135537.mlab.com:35537/ujjwala_home_db");
